17a3744103f2c19ea71b7e477f9fea079c455b15
Anastasia <nastya.k.r@mail.ru>
2017-12-13 18:56:33 +0300

    some changes

--- src/reducers/metro.js
+++ src/reducers/metro.js
@@ -3,24 +3,25 @@ const metro = [
     fill: "#E50520",
     name: "Сокольническая",
     ways: [
-      {id: "w100", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
+      // {id: "w100", is_active: false, class: "st4", x: 1008, y: 199.8, width: 4.4, height: 7.6},
       {id: "w101", is_active: false, class: "st4", x: 995.6, y: 209.9, width: 17.6, height: 4.4,
         transform: "matrix(0.7054 -0.7088 0.7088 0.7054 145.5673 774.382)"},
       {id: "w102", is_active: false, class: "st4", x: 974.1, y: 226.1, width: 28.2, height: 4.4,
         transform: "matrix(0.7054 -0.7088 0.7088 0.7054 129.2664 767.7036)"},
       {id: "w103", is_active: false, class: "st4", x: 960.4, y: 243.5, width: 20.9, height: 4.4,
         transform: "matrix(0.7054 -0.7088 0.7088 0.7054 111.8526 760.5135)"},
-      {id: "w101", is_active: false, class: "st4", x: 995.6, y: 209.9, width: 17.6, height: 4.4,
+      {id: "w104", is_active: false, class: "st4", x: 943.6, y: 259.2, width: 23.4, height: 4.4,
+        transform: "matrix(0.7054 -0.7088 0.7088 0.7054 96.1659 754.1168)"},
+      {id: "w105", is_active: false, class: "st4", x: 924.4, y: 276.9, width: 26.5, height: 4.4,
+        transform: "matrix(0.7054 -0.7088 0.7088 0.7054 78.4157 746.8129)"},
+      {id: "w106", is_active: false, class: "st4", x: 995.6, y: 209.9, width: 17.6, height: 4.4,
         transform: "matrix(0.7054 -0.7088 0.7088 0.7054 145.5673 774.382)"},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
-      {id: "w10", is_active: false, class: "st4", x: 1007.6, y: 199.8, width: 4.4, height: 7.6},
+      {id: "w102", is_active: false, class: "st4", x: 974.1, y: 226.1, width: 28.2, height: 4.4,
+        transform: "matrix(0.7054 -0.7088 0.7088 0.7054 129.2664 767.7036)"},
+      {id: "w103", is_active: false, class: "st4", x: 960.4, y: 243.5, width: 20.9, height: 4.4,
+        transform: "matrix(0.7054 -0.7088 0.7088 0.7054 111.8526 760.5135)"},
+      {id: "w104", is_active: false, class: "st4", x: 943.6, y: 259.2, width: 23.4, height: 4.4,
+        transform: "matrix(0.7054 -0.7088 0.7088 0.7054 96.1659 754.1168)"},
 
     ],
     stations: [
@@ -115,11 +116,33 @@ const metro = [
   }
 ];
 
+{/*<rect x="887.5" y="303.1" transform="matrix(0.7054 -0.7088 0.7088 0.7054 52.1468 736.0893)" class="st4" width="48.2" height="4.4"/>*/}
 
+  {/*<rect x="866.8" y="331.7" transform="matrix(0.7054 -0.7088 0.7088 0.7054 23.4822 724.3267)" class="st4" width="32.6" height="4.4"/>*/}
 
-{/*<rect x="960.4" y="243.5" transform="matrix(0.7054 -0.7088 0.7088 0.7054 111.8526 760.5135)" class="st4" width="20.9" height="4.4"/>*/}
+  {/*<rect x="806.6" y="370.2" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -15.1148 708.4506)" class="st4" width="76.3" height="4.4"/>*/}
 
-  {/*<rect x="943.6" y="259.2" transform="matrix(0.7054 -0.7088 0.7088 0.7054 96.1659 754.1168)" class="st4" width="23.4" height="4.4"/>*/}
+  {/*<rect x="765.4" y="419.1" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -64.0584 688.4274)" class="st4" width="61.6" height="4.4"/>*/}
+
+  {/*<rect x="677.5" y="481.2" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -126.3257 662.896)" class="st4" width="113.6" height="4.4"/>*/}
+
+  {/*<rect x="667.6" y="532.5" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -177.7285 641.7784)" class="st4" width="31.3" height="4.4"/>*/}
+
+  {/*<rect x="640.7" y="556.8" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -202.0455 631.911)" class="st4" width="36.9" height="4.4"/>*/}
+
+  {/*<rect x="596.6" y="590.2" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -235.5547 618.1027)" class="st4" width="58.4" height="4.4"/>*/}
+
+  {/*<rect x="553.1" y="632.5" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -277.933 600.7253)" class="st4" width="61.2" height="4.4"/>*/}
+
+  {/*<rect x="539.2" y="663.6" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -309.0492 588.0327)" class="st4" width="27.3" height="4.4"/>*/}
+
+  {/*<rect x="520.1" y="682.7" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -328.2068 580.1014)" class="st4" width="27.4" height="4.4"/>*/}
+
+  {/*<rect x="505" y="700.4" transform="matrix(0.7054 -0.7088 0.7088 0.7054 -345.9198 572.8421)" class="st4" width="22.4" height="4.4"/>*/}
+  {/*<rect x="506.6" y="709" class="st4" width="4.4" height="24"/>*/}
+  {/*<rect x="506.6" y="733" class="st4" width="4.4" height="18.3"/>*/}
+  {/*<rect x="506.6" y="751.3" class="st4" width="4.4" height="18.3"/>*/}
+  {/*<rect x="506.6" y="769.7" class="st4" width="4.4" height="18.3"/>*/}
 
 export default function login(state = metro, action) {
 
